294d66605a16b01d3cf9204b99a5a7a26c9443d4
Laderlappen <elmervermeulen@gmail.com>
2022-01-28 14:10:56 +0200

    Added solidity compiler version and package for balancer

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,6 +19,10 @@ module.exports = {
     solidity: {
         compilers: [
             {
+                version: "0.7.6",
+                settings: {},
+            },
+            {
                 version: "0.5.17",
             },
             {
