8db95178db3b09b54df2990dcbdbfef84446fb12
Laderlappen <elmervermeulen@gmail.com>
2022-01-28 08:53:01 +0200

    Updated Levr.so to new version. Fixed starting point.

--- contracts/Levr.sol
+++ contracts/Levr.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "contracts/OZv8/ERC20.sol";
+import "contracts/OZv8/Burnable.sol";
+import "contracts/OZv8/AccessControl.sol";
 
 contract LEVR is ERC20, ERC20Burnable, AccessControl 
 {
